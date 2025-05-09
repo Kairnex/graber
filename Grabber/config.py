9fd5a2ba691fc40d633004982f360c0f5de7615e
Kairnex <codexkairnex@gmail.com>
2025-05-09 17:31:22 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -1,15 +1,15 @@
 import os
 
-OWNER_ID = 7455169019
-GROUP_ID = "-1002225496870"
-TOKEN = os.getenv("BOT_TOKEN")
-api_id = 20457610
-api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
+OWNER_ID = 6999372290
+GROUP_ID = "-1002175678780"
+TOKEN = os.getenv("BOT_TOKEN","7810847448:AAGVuvQDkUbdNe9r4XM1G4q3OiAVakJXrms")
+api_id = 21189715
+api_hash = "988a9111105fd2f0c5e21c2c2449edfd"
 PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
-SUPPORT_CHAT = "dragona_support"
-UPDATE_CHAT = "PickBotUpdatesHQ"
-BOT_USERNAME = "Okarun_Game_bot"
-CHARA_CHANNEL_ID = -1002235251549
-MONGO_URL = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
+SUPPORT_CHAT = "Friend_Chat_International"
+UPDATE_CHAT = "pahadichat"
+BOT_USERNAME = "GrabYourWaifyBot"
+CHARA_CHANNEL_ID = -1002647576736
+MONGO_URL = "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "Character_catcher"
-LOG_CHAT_ID = -1002203193964
+LOG_CHAT_ID = -1002647576736
